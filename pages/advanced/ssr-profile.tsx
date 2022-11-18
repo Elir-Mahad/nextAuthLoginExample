6840743283d2c1bf5699bd0bcfecf87c0be77ac2
elir-mahad <mmahad280@gmail.com>
2022-11-18 14:13:03 -0500

    first commit

--- pages/advanced/ssr-profile.tsx
+++ pages/advanced/ssr-profile.tsx
@@ -1,10 +1,10 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0'
-import Layout from '../../components/layout'
-import { User } from '../../interfaces'
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import Layout from "../../components/layout";
+import { User } from "../../interfaces";
 
 type ProfileProps = {
-  user: User
-}
+  user: User;
+};
 
 export default function Profile({ user }: ProfileProps) {
   return (
@@ -13,14 +13,15 @@ export default function Profile({ user }: ProfileProps) {
 
       <div>
         <h3>Profile (server rendered)</h3>
-        <img src={user.picture} alt="user picture" />
+        <img src="https://preview.redd.it/yajawa4x8nw41.jpg?width=640&crop=smart&auto=webp&s=fddc3d63722428710b08e4f4032d082ebc11aa80" />
+        {/* <img src={user.picture} alt="user picture" /> */}
         <p>nickname: {user.nickname}</p>
         <p>name: {user.name}</p>
       </div>
     </Layout>
-  )
+  );
 }
 
 // Protected route, checking authentication status before rendering the page.(SSR)
 // It's slower than a static page with client side authentication
-export const getServerSideProps = withPageAuthRequired()
+export const getServerSideProps = withPageAuthRequired();
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,10 +1,10 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0'
-import Layout from '../components/layout'
-import { User } from '../interfaces'
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import Layout from "../components/layout";
+import { User } from "../interfaces";
 
 type ProfileCardProps = {
-  user: User
-}
+  user: User;
+};
 
 const ProfileCard = ({ user }: ProfileCardProps) => {
   return (
@@ -13,21 +13,22 @@ const ProfileCard = ({ user }: ProfileCardProps) => {
 
       <div>
         <h3>Profile (client rendered)</h3>
-        <img src={user.picture} alt="user picture" />
+        <img src="https://sportshub.cbsistatic.com/i/2021/08/09/b6fa15e8-0a86-4c77-b602-bb22b59c4bac/naruto-kakashi-cosplay-1274250.jpg" />
+        {/* <img src={user.picture} alt="user picture" /> */}
         <p>nickname: {user.nickname}</p>
         <p>name: {user.name}</p>
       </div>
     </>
-  )
-}
+  );
+};
 
 const Profile = ({ user, isLoading }) => {
   return (
     <Layout user={user} loading={isLoading}>
       {isLoading ? <>Loading...</> : <ProfileCard user={user} />}
     </Layout>
-  )
-}
+  );
+};
 
 // Protected route, checking user authentication client-side.(CSR)
-export default withPageAuthRequired(Profile)
+export default withPageAuthRequired(Profile);
